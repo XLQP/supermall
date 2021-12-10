27c373146631865b27aa8989ce9c2436a9f28e94
qiping <1757805272@qq.com>
2021-12-10 11:43:04 +0800

    更新了文档

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router/index'
 import store from "./store/index.js"
 
 import FastClick from "fastclick"
+import VueLazyLoad from "vue-lazyload"
 
 import toast from "components/common/toast/index.js"
 
@@ -16,6 +17,11 @@ Vue.use(toast)
 
 // 解决移动端300ms延迟
 FastClick.attach(document.body)
+
+// 使用懒加载插件
+Vue.use(VueLazyLoad,{
+  loading:require("assets/img/common/placeholder.png")
+})
 
 new Vue({
   render: h => h(App),
